14e9dd30fa9caeeec9856e72bd7b268ca70bfd91
Rajan Saini <rajansaini691@gmail.com>
2018-09-18 20:08:07 -0700

    Initial commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class Test extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (<div> Rajan's Awesome Site! </div>);
+    }
+}
